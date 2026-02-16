bb2ce6d10c91c70c991e192fd0b604b38442afcd
Gabriel J. Csapo <gabecsapo@gmail.com>
2026-02-15 19:06:19 -0500

    chore: upgrade to vite@8 - incredible speed benefits - removes cloudflare worker builder, not needed

--- cf/entry.rsc.tsx
+++
@@ -1,7 +0,0 @@
-import { fetchServer } from "../react-router-vite/entry.rsc";
-
-export default {
-  fetch(request: Request) {
-    return fetchServer(request);
-  },
-};
--- cf/entry.ssr.tsx
+++
@@ -1,7 +0,0 @@
-import handler from "../react-router-vite/entry.ssr";
-
-export default {
-  async fetch(request: Request, env: any) {
-    return handler(request, await env.RSC.fetch(request));
-  },
-};
